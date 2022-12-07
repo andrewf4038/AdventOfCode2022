249344b165202441b419f20a2bab1ebb664a64e5
Andrew Fowler <andrewf4038@gmail.com>
2022-12-07 11:16:20 -0600

    changed names for part one and part two differences

--- adventOfCode2022/Day3/index.js
+++ adventOfCode2022/Day3/index.js
@@ -3,21 +3,21 @@ const text = fs.readFileSync("./input.txt", encoding = "utf8");
 const myArray = text.split("\n");
 
 const priorityNumber = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var prioritySum = 0;
+var partOneSum = 0;
 
 myArray.forEach(string => {
     firstCompartment = string.substring(0, string.length / 2);
     secondCompartment = string.substring(string.length / 2);
-    stringCompare(firstCompartment, secondCompartment);
+    compartmentCompare(firstCompartment, secondCompartment);
 }
 )
-console.log(prioritySum);
+console.log("Solution to part 1 is %d", partOneSum);
 
 
-function stringCompare(string1, string2) {
+function compartmentCompare(string1, string2) {
     for (const i in string1) {
         if (string2.includes(string1[i]) === true) {
-            return prioritySum += priorityNumber.indexOf(string1[i]) + 1;
+            return partOneSum += priorityNumber.indexOf(string1[i]) + 1;
         }
     }
 }
