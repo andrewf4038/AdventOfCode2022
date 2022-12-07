2fabcd03856e60441bf2b96f22cf4cf2aa6c8614
Andrew Fowler <andrewf4038@gmail.com>
2022-12-07 13:51:14 -0600

    Day 3

--- adventOfCode2022/Day3/index.js
+++ adventOfCode2022/Day3/index.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const { nextTick } = require("process");
 const text = fs.readFileSync("./input.txt", encoding = "utf8");
 const myArray = text.split("\n");
 
@@ -9,31 +10,30 @@ var sameItem = "";
 myArray.forEach(rucksack => {
     firstCompartment = rucksack.substring(0, rucksack.length / 2);
     secondCompartment = rucksack.substring(rucksack.length / 2);
-    let { item } = compareItem(firstCompartment, secondCompartment);
+    let item = compareItem(firstCompartment, secondCompartment);
     partOneSum += itemPriority(item);
-    // partOneSum += compartmentComparePriority(firstCompartment, secondCompartment);
 }
-)
+);
 console.log("Solution to part 1 is %d", partOneSum);
 
+var partTwoSum = 0;
 const groups = [];
 groupReducer(myArray);
-console.log(groups[0])
 groups.forEach(group => {
-    let { item, itemIndex } = compareItem(group[0], group[1]);
-    console.log(item);
-    console.log(itemIndex);
+    let item = compareItem(group[0], group[1], group[2]);
+    partTwoSum += itemPriority(item);
 });
+console.log("Solution to part 2 is %d", partTwoSum);
 
 
 
-function compareItem(string1, string2, iter = 0) {
-    for (let i = iter; i < string1.length && i < string2.length; i++) {
-        if (string2.includes(string1[i]) === true) {
-            return {
-                'item': string1[i],
-                'itemIndex': i
-            }
+
+function compareItem(string1, string2, string3 = "", iter = 0) {
+    for (let i = iter; i < string1.length || i < string2.length || i <= string3.length; i++) {
+        if (string3 === "" && string2.includes(string1[i])) {
+            return string1[i];
+        } else if (string2.includes(string1[i]) && string3.includes(string1[i])) {
+            return string1[i];
         }
     }
 }
