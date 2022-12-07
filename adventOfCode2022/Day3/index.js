308431bf5dc0e638058afed844a5c7c71b6d3eec
Andrew Fowler <andrewf4038@gmail.com>
2022-12-07 12:20:53 -0600

    Before changes to the compare function

--- adventOfCode2022/Day3/index.js
+++ adventOfCode2022/Day3/index.js
@@ -1,4 +1,6 @@
+const { group } = require("console");
 const fs = require("fs");
+const { arrayBuffer } = require("stream/consumers");
 const text = fs.readFileSync("./input.txt", encoding = "utf8");
 const myArray = text.split("\n");
 
@@ -8,16 +10,35 @@ var partOneSum = 0;
 myArray.forEach(string => {
     firstCompartment = string.substring(0, string.length / 2);
     secondCompartment = string.substring(string.length / 2);
-    compartmentCompare(firstCompartment, secondCompartment);
+    partOneSum += compartmentComparePriority(firstCompartment, secondCompartment);
 }
 )
 console.log("Solution to part 1 is %d", partOneSum);
 
+const groups = []
+groupReducer(myArray);
+groups.forEach(group => {
+    // Compare group 1 and group 2 and find one that is the same
+    // compare group 2 and group 3 and see if that value is in there?
+});
 
-function compartmentCompare(string1, string2) {
+console.log(groups[0])
+
+// console.log(myArray.slice(0, 3));
+// console.log("----");
+// console.log(myArray.slice(3, 6));
+
+function compartmentComparePriority(string1, string2) {
     for (const i in string1) {
         if (string2.includes(string1[i]) === true) {
-            return partOneSum += priorityNumber.indexOf(string1[i]) + 1;
+            return priorityNumber.indexOf(string1[i]) + 1;
         }
+    }
+}
+
+function groupReducer(array) {
+    groups.push(array.splice(0, 3));
+    if (array.length >= 3) {
+        groupReducer(array);
     }
 }
