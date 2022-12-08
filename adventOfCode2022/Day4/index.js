3dcdb35fb9d65a1709ce9a4e6b638c656a7fe70f
Andrew Fowler <andrewf4038@gmail.com>
2022-12-08 16:47:55 -0600

    Day 4

--- adventOfCode2022/Day4/index.js
+++ adventOfCode2022/Day4/index.js
@@ -4,16 +4,40 @@ const text = fs.readFileSync("./input.txt", encoding = "utf8");
 const myArray = text.split("\n");
 
 var sameSectionCount = 0;
+var partialOverlapCount = 0;
 
-const allPairs = [];
+const pairs = [];
 myArray.forEach(pair => {
-    allPairs.push(pair.split(","))
+    pairs.push(pair.split(","))
 })
-console.log(allPairs);
 
-allPairs.forEach(assignments => {
-    assignments.forEach(assignment => {
-        assignment = assignment.split("-");
-        console.log(Number(assignment[1]) - Number(assignment[0]));
-    })
-})
+pairs.forEach(pair => sameSectionCount += fullOverlapCompare(pair));
+console.log("The solution for part 1 is %d", sameSectionCount);
+
+pairs.forEach(pair => partialOverlapCount += partialOverlapCompare(pair));
+console.log("The solution for part 2 is %d", partialOverlapCount);
+
+function fullOverlapCompare(pair) {
+    const pairOneLow = Number(pair[0].split("-")[0]);
+    const pairOneHigh = Number(pair[0].split("-")[1]);
+    const pairTwoLow = Number(pair[1].split("-")[0]);
+    const pairTwoHigh = Number(pair[1].split("-")[1]);
+
+    if ((pairOneLow <= pairTwoLow && pairOneHigh >= pairTwoHigh) || (pairTwoLow <= pairOneLow && pairTwoHigh >= pairOneHigh)) {
+        return 1
+    } else return 0
+}
+
+function partialOverlapCompare(pair) {
+    const pairOneLow = Number(pair[0].split("-")[0]);
+    const pairOneHigh = Number(pair[0].split("-")[1]);
+    const pairTwoLow = Number(pair[1].split("-")[0]);
+    const pairTwoHigh = Number(pair[1].split("-")[1]);
+
+    if (pairOneLow > pairTwoHigh) {
+        return 0
+    } else if (pairOneHigh < pairTwoLow) {
+        return 0
+    } else return 1
+
+}
